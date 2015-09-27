7329137ff0de80316a9b92de9f31dafc9d1577f1
Fernando Perez Tavares <fernandoperez21@gmail.com>
2015-09-26 22:44:49 -0300

    Update cachematrix.R
    Commit for lesson 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This function does the inverse matrix optimizing the processing 
+# Obs: This assumes that the matrix is always invertible
 
-## Write a short comment describing this function
+# makeCacheMatrix is a function (like a class) that creates a list containing
+# functions to:
+#  A - SET the value of the matrix
+#  B - GET the value of the matrix
+#  C - SET the value of inverse of the matrix
+#  D - GET the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set=set, get=get, setinverse=setinverse, 
+             getinverse=getinverse)
 }
 
+# This function returns the inverse of the matrix doing:
+# A - Checks if the inverse has already been computed.
+# B - If YES, gets the cached result and skips the computation.
+# C - If NOT, computes the inverse, sets the value in the cache via
+#     setinverse function (above).
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data.")
+                return(inv)
+        }
+        data <- x$get()
+        # Note: solve function does the inverse of the matrix (square)
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
 }
+
+
+## To test this function RUN:
+
+# x = rbind(c(10, -1/3), c(1/4, 25))
+# m = makeCacheMatrix(x)
+# m$get()
+# cacheSolve(m)
+# cacheSolve(m)
+
+## Note that in the first run there was no cache, but in the second run (and 
+## the next ones) the function got the cached solution
